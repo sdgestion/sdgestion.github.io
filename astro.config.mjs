d475bf20683f67c66b4d4fcedee6bcee42b89832
Kevin Sánchez <5795125+kevinxe@users.noreply.github.com>
2025-08-10 23:48:05 +0200

    Changes in Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://sdgestion.github.io',
+  site: 'https://www.sdgestion.es',
   integrations: [
     tailwind(),
     sitemap()
